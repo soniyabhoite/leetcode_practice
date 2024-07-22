81cea9e55c5d833d1b5b42b2cf44b93af684a4c8
soniyabhoite <soniyabhoite223@gmail.com>
2024-07-22 20:11:27 +0530

    updates

---
+++ nimap.js
@@ -0,0 +1,22 @@
+function test()
+{
+   // console.log("a"+"b" + + "c" + "d") //abNaNd
+   // console.log(a)
+    //let a = "abc" //Cannot access 'a' before initialization
+    //var a="abc"   //undefined
+    //const a="abc"  //Cannot access 'a' before initialization
+   // console.log(console.log("abc"))   //abc  undefined
+
+   console.log(NaN == NaN)     //false    NaN is not a valid number, and thus cannot be compared to any other value, even to itself.
+
+    
+
+
+}
+test()
+
+hii()
+function hii(){
+    console.log("hello")    // print hello
+}
+
